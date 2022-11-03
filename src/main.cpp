6093efa7720584a19c51adbb6f47c471f49a13d8
monolith-kh <kh.cho@monolith.co.kr>
2022-11-03 13:50:48 +0900

    add log message (debug and info)

---
+++ src/main.cpp
@@ -0,0 +1,11 @@
+#include <Arduino.h>
+
+void setup() {
+  // put your setup code here, to run once:
+  log_i("Hello esp32");
+  log_d("verbose message");
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+}
